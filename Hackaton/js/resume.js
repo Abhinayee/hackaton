3e55b4f6e3c1c3868237b136bc23035d20e8792a
Abhinayee <120196520+Abhinayee@users.noreply.github.com>
2022-12-29 22:30:57 -0800

    Add files via upload

---
+++ Hackaton/js/resume.js
@@ -0,0 +1,19 @@
+function resume(){
+    let firstname;
+    firstname=document.getElementById('firstname').value;
+    let middlename;
+    middlename=document.getElementById('middlename').value;
+    let lastname;
+    lastname=document.getElementById('lastname').value;
+    document.getElementById('Name').innerHTML=firstname+middlename+lastname;
+    let EmailId;
+    EmailId=document.getElementById('mail').value;
+    document.getElementById('EmailId').innerHTML=EmailId;
+    let DOB;
+    DOB=document.getElementById('DateOfBirth').value;
+    document.getElementById('DOB').innerHTML=DOB;
+    let PhoneNumber;
+    PhoneNumber=document.getElementById('MobileNumber').value;
+    document.getElementById('PhoneNumber').innerHTML=PhoneNumber;
+
+}
